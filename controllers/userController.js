6c64338cccddfd9309d729a0cbde62472a1cf423
Hariiikrishnan <therihari36@gmail.com>
2024-03-17 22:15:04 +0530

    added validations

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,27 +16,41 @@ app.use(bodyparser.urlencoded({ extended: true }));
 
 const registerUser = asyncHandler(async(req,res)=>{
 
+  const username = req.body.username;
+    const password = req.body.password;
+    const rePassword = req.body.rePassword;
+    const email = req.body.email;
+    const user = new User({
+      username: username,
+      password: password,
+      
+    });
+    var emailFormat = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 
+    if( email.match(emailFormat) || (username!=="" || password !=="" || rePassword!=="" || email!=="") || (rePassword!==password)){
+      res.render("error");
+    }else{
 
-    User.register(
+      User.register(
         { u_id: uuidv4(), username: req.body.username,
           email:req.body.email,
           // isAdmin:true,
-          isManager:true,
+          // isManager:true,
           },
           
         req.body.password,
         function (err, user) {
           if (err) {
             console.log(err);
-            res.sendStatus(500);
+            // res.sendStatus(500);
+            res.render("error");
             return;
           } else {
            
             passport.authenticate("local")(req, res, function () {
      
               jwt.sign({ user }, process.env.SECRETKEY, (err, token) => {
-      
+                
                 User.findOne({ username: req.body.username }).then((result)=>{
                 
                   console.log(result);
@@ -62,7 +76,8 @@ const registerUser = asyncHandler(async(req,res)=>{
             });
           }
         }
-      );
+        );
+      }
 });
 
 
@@ -74,33 +89,40 @@ const loginUser = asyncHandler(async(req,res)=>{
       password: password,
     });
 
-    // console.log(user);
-  
-    req.login(user, function (err) {
-      if (err) {
-        console.log(err);
+    if( username === "" || password === ""){
+      console.log("here");
+      res.render("error");
+    }else{
+
+      req.login(user, function (err) {
+        if (err) {
+          console.log(err);
         
-        res.render("login",{status:500})
+        // console.log(res.statusCode);
+        res.render("login",{status:500});
         return;
       } else {
         passport.authenticate("local")(req, res, function () {
-            
-
-            jwt.sign({ user }, process.env.SECRETKEY, (err, token) => {
-              // console.log(token);
-              User.findOne({ username: username }).then((result)=>{
-                
-                console.log(result);
-                if(result.isManager){
-                  res.redirect('/manager/'+result.u_id);
-                }
-                else if(result.isAdmin){
-                  res.redirect('/admin/'+result.u_id);
+           
+          
+          jwt.sign({ user }, process.env.SECRETKEY, (err, token) => {
+            // console.log(token);
+            User.findOne({ username: username }).then((result)=>{
+              
+              console.log(result);
+              if(result.isManager){
+                res.redirect('/manager/'+result.u_id);
+              }
+              else if(result.isAdmin){
+                res.redirect('/admin/'+result.u_id);
+              }
+                else if(res.statusCode === 400){
+                  res.render("error");
                 }
                 else{
                   res.redirect("home");
                 }
-
+                
                 // console.log(result);
                 
                 
@@ -113,6 +135,7 @@ const loginUser = asyncHandler(async(req,res)=>{
           });
         }
       });
+    }
   
     })
     
